b0e0caa7a52b46d025d75313654c120bfdaf73e3
Teddy <teddytp330@gmail.com>
2025-11-22 13:19:10 +0630

    make seperate model

--- api/signup.js
+++ api/signup.js
@@ -1,32 +1,29 @@
-const mongoose = require("mongoose");
-//const bcrypt = require("bcryptjs");
+import mongoose from "mongoose";
+import User from "../models/User.js";
 
-const MONGO_URL = process.env.MONGO_URL;
+let isConnected = false;
 
-let conn = null;
 async function connectDB() {
-  if (conn) return conn;
+  if (isConnected) return;
 
-  conn = await mongoose.connect(MONGO_URL);
-  return conn;
+  await mongoose.connect(process.env.MONGODB_URI);
+  isConnected = true;
 }
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Only POST allowed" });
+  }
+
   await connectDB();
 
   const { username, password } = req.body;
 
-  const User = mongoose.models.User || mongoose.model(
-    "User",
-    new mongoose.Schema({ username: String, password: String })
-  );
-
-  const existing = await User.findOne({ username });
-  if (existing) return res.json({ success: false, message: "User exists" });
-
-  //const hash = await bcrypt.hash(password, 10);
-  const hash = password; // for simple
-  await User.create({ username, password: hash });
-
-  return res.json({ success: true, message: "Reset OK" });
-};
+  try {
+    const newUser = new User({ username, password });
+    await newUser.save();
+    return res.status(201).json({ message: "User created" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
+}
